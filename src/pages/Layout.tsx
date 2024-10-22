fef8886cf855643e45eb04c1e1ba663700f85b05
Elina <elina50505051@yandex.ru>
2024-10-22 19:06:26 +0300

    fix: fix app positioning

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,10 +4,7 @@ import React from 'react';
 import { Container, Paper } from '@mui/material';
 
 export const Layout = (): React.JSX.Element => (
-  <Container
-    maxWidth={'sm'}
-    style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)' }}
-  >
+  <Container maxWidth={'sm'} style={{ margin: '10vh auto' }}>
     <Paper elevation={10} sx={{ padding: '5px' }}>
       <AppHeading />
       <Outlet />
